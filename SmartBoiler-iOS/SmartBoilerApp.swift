d377dd1ab9790e194ce0ffa64ed3d829696d3faf
Leonardo Larrañaga <luis.larranaga@uabc.edu.mx>
2025-02-21 17:17:29 -0800

    Initial Commit

---
+++ SmartBoiler-iOS/SmartBoilerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartBoiler_iOSApp.swift
+//  SmartBoiler-iOS
+//
+//  Created by Leonardo Larrañaga on 2/21/25.
+//
+
+import SwiftUI
+
+@main
+struct KiLLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
